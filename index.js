340dad328d0903ff893e6dabe9fde3a10753166d
LH-Song <115139278+LH-Song@users.noreply.github.com>
2022-12-07 23:58:42 +1000

    js load bug fix try 3

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import './index.css'
 import { typeWrite, changeNavBg, navbar, scrollContainer } from './src/js/common.js'
 
 window.addEventListener('load', typeWrite)
-navbar.addEventListener('scroll', changeNavBg)
+// navbar.addEventListener('scroll', changeNavBg)
 scrollContainer.addEventListener('scroll', changeNavBg)
-window.addEventListener('scroll', changeNavBg)
+// window.addEventListener('scroll', changeNavBg)
