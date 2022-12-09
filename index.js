2ffa063de9cc9129112466bcfecf4879f771fff1
LH-Song <115139278+LH-Song@users.noreply.github.com>
2022-12-10 09:48:34 +1000

    delete outdated

--- index.js
+++
@@ -1,8 +0,0 @@
-
-import { typeWrite, changeNavBg, scrollContainer } from './src/js/common.js'
-
-window.addEventListener('load', typeWrite)
-// navbar.addEventListener('scroll', changeNavBg)
-scrollContainer.addEventListener('scroll', changeNavBg)
-// window.addEventListener('scroll', changeNavBg)
-// window.addEventListener('scroll', changeNavBg)
